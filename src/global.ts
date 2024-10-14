7abf97f434a299c27d254f39629cfcbb54b351b0
Amos Barbato <amosbarbato@gmail.com>
2024-10-14 18:30:33 -0300

    chore: first config

---
+++ src/global.ts
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const color = {
+  dark: '#555555',
+  gray: '#C2C2C2',
+  darkPurple: '#1E2772',
+  purple: '#7001FD',
+  main: '#eee',
+  clean: '#F1F3F6',
+  alert: '#FF5F5F'
+}
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+  body {
+    background: ${color.main};
+  }
+  .medium {
+    font-weight: 500;
+  }
+  .semibold {
+    font-weight: 600;
+  }
+  .bold {
+    font-weight: 700;
+  }
+`
+
+export default Global
